c8aae8214aff1880b22ed65e68ceee415a2ff5f3
Ashwin Nair <ash1995@gmail.com>
2021-06-16 12:33:40 +0400

    Fix cli.py removal introduced in 6be8d49e302d3128e229ce26d91d923874293ef3

---
+++ changedet/cli.py
@@ -0,0 +1,15 @@
+"""Console script for changedet."""
+
+import fire
+
+from changedet.pipeline import ChangeDetPipeline
+
+
+def main():
+    # fire.Fire({"help": help, "test": test, "cdet": cdet})
+    # cdet = ChangeDetPipeline()
+    fire.Fire(ChangeDetPipeline)
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
