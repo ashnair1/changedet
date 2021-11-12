94007bef7bee0fa453c025c7c707bd9ecab9e001
Ashwin Nair <ash1995@gmail.com>
2021-11-12 19:31:30 +0400

    Add test for missing inputs

--- tests/test_changedet.py
+++ tests/test_changedet.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 """Tests for `changedet` package."""
 
+import os
+
 import fire
+import pytest
 
 from changedet.pipeline import ChangeDetPipeline
+
+TEST_DATA = "tests/data"
 
 
 def test_changedet_cli(capsys):
@@ -11,3 +16,11 @@ def test_changedet_cli(capsys):
     captured = capsys.readouterr()
     result = captured.out
     assert "['cva', 'imgdiff', 'ipca', 'irmad']\n" == result
+
+
+def test_changedet_missing_inputs(caplog):
+    img1 = os.path.join(TEST_DATA, "t1.tif")
+    img2 = os.path.join(TEST_DATA, "t2.tiff")
+    with pytest.raises(AssertionError):
+        fire.Fire(ChangeDetPipeline, ["--algo", "imgdiff", "run", img1, img2])
+    assert caplog.records[0].message == "Images not found"
