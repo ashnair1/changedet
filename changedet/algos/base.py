6e402afe9b54c6037ddeabd73221a529299e88fb
Ashwin Nair <ash1995@gmail.com>
2022-05-08 15:48:47 +0400

    Switch order for docs

--- changedet/algos/base.py
+++ changedet/algos/base.py
@@ -7,8 +7,8 @@ import numpy as np
 class MetaAlgo(metaclass=ABCMeta):
     """Base class for an algorithm"""
 
-    @abstractmethod
     @classmethod
+    @abstractmethod
     def run(cls, im1: np.ndarray, im2: np.ndarray, **kwargs: Any) -> np.ndarray:
         """
         Abstract method to run change detection
