11fcc9cbb66dbd4a01e7ec309dddc8e739485514
Ashwin Nair <ash1995@gmail.com>
2022-05-08 14:50:18 +0400

    Remove deprecated abstractclassmethod

--- changedet/algos/base.py
+++ changedet/algos/base.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta, abstractclassmethod
+from abc import ABCMeta, abstractmethod
 from typing import Any
 
 import numpy as np
@@ -7,7 +7,8 @@ import numpy as np
 class MetaAlgo(metaclass=ABCMeta):
     """Base class for an algorithm"""
 
-    @abstractclassmethod
+    @abstractmethod
+    @classmethod
     def run(cls, im1: np.ndarray, im2: np.ndarray, **kwargs: Any) -> np.ndarray:
         """
         Abstract method to run change detection
